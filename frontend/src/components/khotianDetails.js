37d6f8f4689b1f9d1a18f9faa33892e87dd8b51b
Rakib Abdullah <rakibabdullah@Rakibs-MacBook-Air.local>
2023-12-26 04:34:41 +0600

    date format fixed

--- frontend/src/components/khotianDetails.js
+++ frontend/src/components/khotianDetails.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const KhotianDetails = ({singleKhotian}) => {
+
+    // Format date to YYYY-MM-DD
+    const formatDate = (dateString) => {
+      const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+      return new Date(dateString).toLocaleDateString('en-US', options);
+    };
   return (
     <div className="workout-details">
       <h4>{singleKhotian.taskTitle}</h4>
       <p><strong>Task Details :</strong> {singleKhotian.taskDetail}</p>
-      <p><strong>Date :</strong> {singleKhotian.date}</p>
-      <p>{singleKhotian.createdAt}</p>
+      <p><strong>Date :</strong> {formatDate(singleKhotian.date)}</p>
+      <p><strong>Task added at :</strong>{formatDate(singleKhotian.createdAt)}</p>
     </div>
   )
 }
