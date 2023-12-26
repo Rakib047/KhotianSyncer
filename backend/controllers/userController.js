b7ad186e4b9f7649e071686e014e9dd3253c0e82
Rakib Abdullah <rakibabdullah@Rakibs-MacBook-Air.local>
2023-12-27 01:16:42 +0600

    jwt token generated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,11 @@
 const userModel=require("../models/userModel")
+const jwt=require("jsonwebtoken")
+
+//generating jwt(token)
+const createToken = (id) =>{
+    return jwt.sign({_id:id},process.env.SECRET_KEY,{expiresIn: "3d"})
+}
+
 //login user
 const loginUser = async (req,res) =>{
     res.json({msg:"login user success!"})
@@ -9,9 +16,12 @@ const signupUser = async (req,res) =>{
 
     const {email,password} =req.body
     try {
+        //create in database will happen in signupStatic
         const newUser=await userModel.signupStatic(email,password)
 
-        res.status(200).json({email,newUser})
+        const jwtToken=createToken(newUser._id)
+
+        res.status(200).json({email,jwtToken})
     } catch (err) {
         res.status(400).json({error:err.message})
     }
