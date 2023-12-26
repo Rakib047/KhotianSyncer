cc1e2307c422e0c03f32036892145dae70805371
Rakib Abdullah <rakibabdullah@Rakibs-MacBook-Air.local>
2023-12-27 00:44:25 +0600

    sign up and validations

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,16 @@ const loginUser = async (req,res) =>{
 
 //signupUser
 const signupUser = async (req,res) =>{
-    res.json({msg:"signup user success!"})
+
+    const {email,password} =req.body
+    try {
+        const newUser=await userModel.signupStatic(email,password)
+
+        res.status(200).json({email,newUser})
+    } catch (err) {
+        res.status(400).json({error:err.message})
+    }
+
 }
 
 module.exports={
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose=require("mongoose")
+const bcrypt=require("bcrypt")
+const validator=require("validator")
 
 const userSchema=new mongoose.Schema(
     {
@@ -13,5 +15,34 @@ const userSchema=new mongoose.Schema(
         }
     }
 )
+
+//static signup method where we will use hashing for storing password in a safe manner
+userSchema.statics.signupStatic=async function(email,password){
+    //we must use function not arrow one to have this keyword
+    //this bcz we dont have the model yet
+
+    if(!email||!password){
+        throw Error("All fields must be filled")
+    }
+    if(!validator.isEmail(email)){
+        throw Error("You should input a valid email")
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password is not strong enough")
+    }
+
+    const exists=await this.findOne({email})
+
+    if(exists){
+        throw Error("Email already in use!")
+    }
+
+    const salt=await bcrypt.genSalt(10)
+    const hashedPassword=await bcrypt.hash(password,salt)
+
+    const newUser=await this.create({email,password:hashedPassword})
+
+    return newUser
+}
 
 module.exports=mongoose.model("User",userSchema)
