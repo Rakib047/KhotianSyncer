8042bdc62bc57e51b3644088edc2cf5a14e1be26
Rakib Abdullah <rakibabdullah@Rakibs-MacBook-Air.local>
2023-12-28 00:53:41 +0600

    added taskType based filtering

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import axios from "axios";
 import { useKhotianContext } from "../hooks/useKhotianContext";
 import { useAuthContext } from "../hooks/useAuthContext";
@@ -11,6 +11,8 @@ const Home = () => {
 
   const { khotianList, dispatch } = useKhotianContext();
   const { user } = useAuthContext();
+  const [filterType, setFilterType] = useState("All");
+  const [filteredKhotians, setFilteredKhotians] = useState([]);
 
   //this function will be fired once as soon as this component is rendered
   useEffect(() => {
@@ -35,26 +37,41 @@ const Home = () => {
     }
   }, [dispatch, user]);
 
+      //filtering
+    // UseEffect to filter khotians whenever filterType changes
+    useEffect(() => {
+      const filteredList = filterKhotians();
+      setFilteredKhotians(filteredList);
+    }, [filterType, khotianList]);
+
+    const filterKhotians = () => {
+      if (filterType === "All") {
+        return khotianList;
+      } else {
+        return khotianList.filter((khotian) => khotian.taskType === filterType);
+      }
+    };
+
   return (
     <div className="home">
       <div className="khotians">
         <div className="filter-container">
           <select
-            id="filterType"
-            //value={filterType}
-            //onChange={(e) => setFilterType(e.target.value)}
+            //id="filterType"
+            value={filterType}
+            onChange={(e) => setFilterType(e.target.value)}
           >
-            <option value="">All Types</option>
+            <option value="All">All Types</option>
             <option value="CT">CT</option>
-            <option value="Offline">Offline</option>
-            <option value="Online">Online</option>
-            <option value="Assignment">Assignment</option>
-            <option value="Evaluation">Evaluation</option>
+            <option value="OFFLINE">Offline</option>
+            <option value="ONLINE">Online</option>
+            <option value="ASSIGNMENT">Assignment</option>
+            <option value="EVALUATION">Evaluation</option>
           </select>
         </div>
 
-        {khotianList &&
-          khotianList.map((singleKhotian) => (
+        {filteredKhotians &&
+          filteredKhotians.map((singleKhotian) => (
             <KhotianDetails
               key={singleKhotian._id}
               singleKhotian={singleKhotian}
