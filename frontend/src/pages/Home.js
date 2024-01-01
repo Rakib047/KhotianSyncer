97e37b44fa15e627b4aa46111911b3f4cce188fc
Rakib Abdullah <rakibabdullah@Rakibs-MacBook-Air.local>
2024-01-01 22:02:26 +0600

    removed ununsed var 5 hook dependency 2

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -37,33 +37,39 @@ const Home = () => {
     };
 
     if (user) {
-      //function for different filtering
-      const filterKhotians = () => {
-        let currentList = khotianList;
-        if (filterType !== "All") {
-          currentList = currentList.filter(
-            (khotian) => khotian.taskType === filterType
-          );
-        }
-        if (priorityType !== "All") {
-          currentList = currentList.filter(
-            (khotian) => khotian.priority === priorityType
-          );
-        }
-        return currentList;
-      };
-      
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       fetchKhotians();
     }
   }, [dispatch, user, khotianList]);
 
+
   //filtering
   // UseEffect to filter khotians whenever filterType changes
   useEffect(() => {
+    //function for different filtering
+    const filterKhotians = () => {
+      let currentList = khotianList;
+      if (filterType !== "All") {
+        currentList = currentList.filter(
+          (khotian) => khotian.taskType === filterType
+        );
+      }
+      if (priorityType !== "All") {
+        currentList = currentList.filter(
+          (khotian) => khotian.priority === priorityType
+        );
+      }
+      return currentList;
+    };
+
     const filteredList = filterKhotians();
 
     setFilteredKhotians(filteredList);
+
   }, [priorityType, filterType, khotianList]);
+
+  
+
 
   return (
     <div className="home">
@@ -109,12 +115,12 @@ const Home = () => {
       </div>
 
       <div>
-        <div className="calendar-container">
+      <div className="calendar-container">
           {khotianList !== null && (
             <KhotianCalendar khotianList={filteredKhotians} />
           )}
         </div>
-        <KhotianForm />
+        <KhotianForm />  
       </div>
     </div>
   );
