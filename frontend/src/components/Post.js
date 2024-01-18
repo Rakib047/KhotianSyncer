b9c75fbb8a5019f03984783d00bfbcb93bf017bb
Rakib047 <1905047@ugrad.cse.buet.ac.bd>
2024-01-18 16:50:54 +0600

    like problem fixed

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -6,17 +6,18 @@ const Post = ({ postId, userName, content, likes, comments }) => {
   const [isLiked, setIsLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(likes.length);
   const { user } = useAuthContext();
+  
 
   useEffect(() => {
     const likedPosts = JSON.parse(localStorage.getItem("likedPosts")) || [];
 
     // Check if the likedPosts array contains an object with the given userId and postId
     const isPostLiked = likedPosts.some(
-      (like) => like.userId === user.jwtToken && like.postId === postId
+      (like) => like.userId === user.roll && like.postId === postId
     );
 
     setIsLiked(isPostLiked);
-  }, [postId, user.jwtToken]);
+  }, [postId, user.roll]);
 
   const handleLike = async () => {
     try {
@@ -35,7 +36,7 @@ const Post = ({ postId, userName, content, likes, comments }) => {
 
       // Find the index of the post in likedPosts array for the specific user
       const userLikeIndex = likedPosts.findIndex(
-        (like) => like.userId === user.jwtToken && like.postId === postId
+        (like) => like.userId === user.roll && like.postId === postId
       );
 
       if (userLikeIndex !== -1) {
@@ -44,7 +45,7 @@ const Post = ({ postId, userName, content, likes, comments }) => {
         setIsLiked(false);
       } else {
         // Add the like for the specific user
-        likedPosts.push({ userId: user.jwtToken, postId });
+        likedPosts.push({ userId: user.roll, postId });
         setIsLiked(true);
       }
 
