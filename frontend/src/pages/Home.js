84ba3eed5fc4db9f8dd86e692856f38e635f2a5c
Rakib047 <1905047@ugrad.cse.buet.ac.bd>
2024-01-30 21:06:36 +0600

    home revert to original

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,48 +1,52 @@
-import React, { useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
 import axios from "axios";
 import { useKhotianContext } from "../hooks/useKhotianContext";
 import { useAuthContext } from "../hooks/useAuthContext";
-import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 import "react-calendar/dist/Calendar.css";
 import KhotianCalendar from "../components/KhotianCalendar";
+
+//components
 import KhotianDetails from "../components/khotianDetails";
 import KhotianForm from "../components/khotianForm";
 
 const Home = () => {
+  //const [khotianList,setkhotianList]=useState(null) //no longer needed
+
   const { khotianList, dispatch } = useKhotianContext();
   const { user } = useAuthContext();
   const [filterType, setFilterType] = useState("All");
-  const [priorityType, setPriorityType] = useState("All");
+  const [priorityType, setpriorityType] = useState("All");
   const [filteredKhotians, setFilteredKhotians] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
 
-
+  //this function will be fired once as soon as this component is rendered
   useEffect(() => {
-    
     const fetchKhotians = async () => {
       try {
         const response = await axios.get("/api/khotian", {
           headers: { Authorization: `Bearer ${user.jwtToken}` },
         });
 
+
         if (response.status === 200) {
           const json = response.data;
           dispatch({ type: "SET_KHOTIANS", payload: json });
         }
-
-        setIsLoading(false); // Set loading to false when data is fetched
       } catch (error) {
+        // Handle error, e.g., set an error state or dispatch an error action
         console.error("Error fetching khotians:", error);
-        setIsLoading(false); // Set loading to false in case of error
       }
     };
 
     if (user) {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       fetchKhotians();
     }
-  }, [dispatch, user]);
+  }, [dispatch, user, khotianList]);
 
+  //filtering
+  // UseEffect to filter khotians whenever filterType changes
   useEffect(() => {
+    //function for different filtering
     const filterKhotians = () => {
       let currentList = khotianList;
       if (filterType !== "All") {
@@ -66,64 +70,59 @@ const Home = () => {
   return (
     <div>
       <h2 className="headings">
-        <i className="fa-solid fa-list-check"></i> Assessments
+        <i class="fa-solid fa-list-check"></i> Assessments
       </h2>
-      {isLoading ? (
-        <div className="loader-container">
-          <ClimbingBoxLoader color="#1aac83" />
-        </div>
-      ) : (
-        <div className="home">
-          <div className="khotians">
-            <div className="filter-container">
-              <select
-                value={filterType}
-                onChange={(e) => {
-                  setFilterType(e.target.value);
-                }}
-              >
-                <option value="All">All Assessments</option>
-                <option value="CT">CT</option>
-                <option value="OFFLINE">OFFLINE</option>
-                <option value="ONLINE">ONLINE</option>
-                <option value="ASSIGNMENT">ASSIGNMENT</option>
-                <option value="EVALUATION">EVALUATION</option>
-                <option value="THESIS">THESIS WORK</option>
-                <option value="PRESENTATION">PRESENTATION</option>
-                <option value="LAB QUIZ">LAB QUIZ</option>
-              </select>
+      <div className="home">
+        <div className="khotians">
+          <div className="filter-container">
+            <select
+              value={filterType}
+              onChange={(e) => {
+                setFilterType(e.target.value);
+              }}
+            >
+              <option value="All">All Assessments</option>
+              <option value="CT">CT</option>
+              <option value="OFFLINE">OFFLINE</option>
+              <option value="ONLINE">ONLINE</option>
+              <option value="ASSIGNMENT">ASSIGNMENT</option>
+              <option value="EVALUATION">EVALUATION</option>
+              <option value="THESIS">THESIS WORK</option>
+              <option value="PRESENTATION">PRESENTATION</option>
+              <option value="LAB QUIZ">LAB QUIZ</option>
+            </select>
 
-              <select
-                value={priorityType}
-                onChange={(e) => {
-                  setPriorityType(e.target.value);
-                }}
-              >
-                <option value="All">All Priorities</option>
-                <option value="Low">Low</option>
-                <option value="Medium">Medium</option>
-                <option value="High">High</option>
-              </select>
-            </div>
+            <select
+              value={priorityType}
+              onChange={(e) => {
+                setpriorityType(e.target.value);
+              }}
+            >
+              <option value="All">All Priorites</option>
+              <option value="Low">Low</option>
+              <option value="Medium">Medium</option>
+              <option value="High">High</option>
+            </select>
+          </div>
 
-            {filteredKhotians.map((singleKhotian) => (
+          {filteredKhotians &&
+            filteredKhotians.map((singleKhotian) => (
               <KhotianDetails
                 key={singleKhotian._id}
                 singleKhotian={singleKhotian}
               />
             ))}
-          </div>
-
-          <div>
-            <div className="calendar-container">
-              {khotianList !== null && (
-                <KhotianCalendar khotianList={filteredKhotians} />
-              )}
-            </div>
-            <KhotianForm />
-          </div>
         </div>
-      )}
+
+        <div>
+          <div className="calendar-container">
+            {khotianList !== null && (
+              <KhotianCalendar khotianList={filteredKhotians} />
+            )}
+          </div>
+          <KhotianForm />
+        </div>
+      </div>
     </div>
   );
 };
